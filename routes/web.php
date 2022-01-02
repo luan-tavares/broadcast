c79d96711a091caeb6fb3cb8cb9a6a6f0cc1f5ae
Luan <luan.tavares.lourenco@gmail.com>
2022-01-01 22:55:44 -0300

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Events\TestEvent;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    event(new TestEvent());
+
+    return 'oi';
+});
+
+Route::get('/w', function () {
+    return view('welcome');
+});
